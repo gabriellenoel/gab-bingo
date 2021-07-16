a2333b0383bfb338e6a65789a88bb938d401153b
gabriellenoel <gabby@gabriellealexa.com>
2021-07-16 15:44:58 -0400

    final app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
 import BingoCard from './components/BingoCard';
-import Itinerary from './components/Itinerary';
-
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
 
 function App() {
 
